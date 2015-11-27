b5ab56a9363bf993c0d38b228ac70e4a9a96653c
Anshul-Panwar <anshul.s.panwar@gmail.com>
2015-11-28 00:20:57 +0530

    maps activity

--- app/src/main/java/com/example/anshul/findtrains/MapsActivity.java
+++ app/src/main/java/com/example/anshul/findtrains/MapsActivity.java
@@ -1,65 +1,142 @@
 package com.example.anshul.findtrains;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static com.example.anshul.findtrains.MainActivity.*;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+    private void getLatLong(String number) {
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
+
+
+
+        List<String> lat = new ArrayList<String>();
+        List<String> code = new ArrayList<String>();
+        List<String> state = new ArrayList<String>();
+        List<String> lng = new ArrayList<String>();
+        List<String> fullname = new ArrayList<String>();
+
+        try{// start
+            String response = new MainActivity.Task().execute("http://api.railwayapi.com/route/train/" + number + "/apikey/cfpwq4935/").get();
+            JSONObject jsonObject = new JSONObject(response);
+            StringBuilder sb1 = new StringBuilder();
+            JSONArray jsonArray = jsonObject.getJSONArray("route");
+            JSONObject jsonObject2 = jsonObject.getJSONObject("train");
+            JSONArray jsonArray2 = jsonObject2.getJSONArray("days");
+            for (int x = 0; x < jsonArray.length(); x++) {
+
+                lat.add(jsonArray.getJSONObject(x).getString("lat"));
+                state.add(jsonArray.getJSONObject(x).getString("state"));
+                lng.add(jsonArray.getJSONObject(x).getString("lng"));
+                fullname.add(jsonArray.getJSONObject(x).getString("fullname"));
+
+
+
             }
+        }
+        catch(Exception e)
+        {
+
+
+
+
+
+        }
+
+
+
+        for(int i=0;i<lat.size();i++) {
+            Toast.makeText(MapsActivity.this, "" + lat.get(i), Toast.LENGTH_LONG
+            ).show();
+            if(Double.parseDouble(lat.get(i))!=0.0&&!fullname.get(i).contains("NEW DELHI")) {
+                if (i == 0) {
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Double.parseDouble(lat.get(i)), Double.parseDouble(lng.get(i))), 10));
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(lat.get(i)), Double.parseDouble(lng.get(i)))).title(fullname.get(i)).snippet(state.get(i)));
+                } else {
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(lat.get(i)), Double.parseDouble(lng.get(i)))).title(fullname.get(i)).snippet(state.get(i)));
+
+                }
+            }
+            else if(fullname.get(i).contains("NEW DELHI"))
+            {
+                lat.set(i,"28.643");
+                lng.set(i,"77.222");
+                state.set(i,"Delhi");
+                mMap.addMarker(new MarkerOptions().position(new LatLng(28.643,77.222)).title(fullname.get(i)).snippet(state.get(i)));
+
+            }
+
+
+        }
+
+
+
+
+
+        for(int i=0;i<lat.size()-1;)
+        {
+
+            int x=i;
+            int y;
+            if(Double.parseDouble(lat.get(i+1))!=0.0) {
+
+                y=i+1;
+            }
+            else
+            {
+                y=i+2;
+            }
+
+
+
+
+            mMap.addPolyline(new PolylineOptions().add(new LatLng(Double.parseDouble(lat.get(x)),Double.parseDouble(lng.get(x))),new LatLng(Double.parseDouble(lat.get(y)),Double.parseDouble(lng.get(y)))));
+            i=y;
+
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        String response = getIntent().getExtras().getString("response");
+        Toast.makeText(MapsActivity.this, response, Toast.LENGTH_SHORT).show();
+        getLatLong(response);
+
     }
 }
--- app/src/main/java/com/example/anshul/findtrains/Train.java
+++ app/src/main/java/com/example/anshul/findtrains/Train.java
@@ -1,5 +1,5 @@
 package com.example.anshul.findtrains;
-
+import java.util.*;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,19 +9,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class Train extends Activity {
     StringBuilder sb;
     EditText et,et1;
     Button a, b;
-    TextView tv;
+    TextView tv;String res,num;
+    List<String> number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +42,50 @@ public class Train extends Activity {
 
         a.setOnClickListener(new View.OnClickListener() {
                                  @Override
-                                 public void onClick(View v) {
-                                     try {
-                                         String res=new Task().execute(et.getText().toString(),et1.getText().toString()).get();
+                                  public void onClick(View v) {
+                                                                          try {
+           String res=new Task().execute("http://api.railwayapi.com/between/source/" + et.getText().toString() + "/dest/" + et1.getText().toString() + "/date/27-11-2015/apikey/makkf7604/").get();
+
+
+
+
+                     List<String> dest_arr, sour_dept, to, from, name, number;
+                     dest_arr = new ArrayList<>();
+                      sour_dept = new ArrayList<>();
+                      to = new ArrayList<>();
+                      from = new ArrayList<>();
+                       name = new ArrayList<>();
+                      number = new ArrayList<>();
+
+
+                      JSONObject jsonObject = new JSONObject(res);
+                      JSONArray jsonArray = jsonObject.getJSONArray("train");
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                      JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                      dest_arr.add(jsonObject1.getString("dest_arrival_time"));
+                      sour_dept.add(jsonObject1.getString("src_departure_time"));
+                      to.add(jsonObject1.getJSONObject("to").getString("name"));
+                      from.add(jsonObject1.getJSONObject("from").getString("name"));
+                      name.add(jsonObject1.getString("name"));
+                      number.add(jsonObject1.getString("number"));
+
+                                             }
+
+            RecyclerAdapter rad = new RecyclerAdapter(TrainBetween.this, name, number, dest_arr, sour_dept, to, from);
+
+             StaggeredGridLayoutManager lm = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+
+                     rv.setLayoutManager(lm);
+                     rv.setHasFixedSize(true);
+                     rv.setAdapter(rad);
+
+
+                      for (int z = 0; z < name.size(); z++) {
+                       Toast.makeText(Train.this, name.get(z), Toast.LENGTH_SHORT).show();
+                             }
+
+                          //Toast.makeText(TrainBetween.this, "" + train, Toast.LENGTH_SHORT).show();
 
 
 
